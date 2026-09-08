83e218e741ad1fa15be80a7ca12524635dfbf9c3
admluminaresestetica-cloud <admluminaresestetica@gmail.com>
2026-09-08 05:51:23 -0300

    Actualizar ModalDetalleProducto.tsx

--- src/app/tienda/components/ModalDetalleProducto.tsx
+++ src/app/tienda/components/ModalDetalleProducto.tsx
@@ -288,17 +288,18 @@ export default function ModalDetalleProducto({
               </button>
 
               {/* Botón de Comprar Ahora (Estilo Mercado Libre, Limpio) */}
-              <button
+             <button
   onClick={handleComprarAhora}
   disabled={sinStock || maximoPermitidoParaAgregar <= 0}
   className={`flex w-full items-center justify-center gap-2 rounded-2xl py-3.5 text-sm font-bold transition-all mt-2.5 ${
     sinStock || maximoPermitidoParaAgregar <= 0
-      ? "bg-slate-200 text-slate-400 cursor-not-allowed"
-      : "bg-[#12151B] text-white hover:bg-[#12151B]/90 active:scale-[0.98] shadow-md shadow-[#12151B]/10 cursor-pointer"
+      ? "bg-slate-200 text-slate-400 cursor-not-allowed border-transparent"
+      : "border-2 border-[#0E6E55] bg-transparent text-[#0E6E55] hover:bg-[#0E6E55]/10 active:scale-[0.98] cursor-pointer"
   }`}
 >
   <span>Comprar ahora</span>
 </button>
+
 
 
             </div>
